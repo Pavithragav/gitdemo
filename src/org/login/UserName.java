ecc82abf4b6ec87244b9fc0230a8398e5fe2064a
Pavithragav <pavithra.mba01@gmail.com>
2022-06-18 16:25:33 +0530

    Git comments

---
+++ src/org/login/UserName.java
@@ -0,0 +1,31 @@
+package org.login;
+
+//class name
+public class UserName {
+
+	//method (ctrl + space)
+	private void phoneNo() {
+		System.out.println("123456789");
+	
+
+}
+	
+	private void email() {
+		System.out.println("pavithra.mba@gmail.com");
+	
+	
+}
+//main method
+
+public static void main(String[] args) {
+	
+	//object creation
+	
+	UserName test= new UserName();
+			
+			//method calling
+			test.phoneNo();
+	test.email();
+	
+}
+}
